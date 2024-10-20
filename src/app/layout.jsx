adce925cf6dcd999c3a7e5dd56c13947f0d2badb
이재호 <apple@appleui-MacBookPro.local>
2024-10-20 23:37:50 +0900

    header 작업 완

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,14 @@
+import '@/styles/css/style.css';
+import Header from "@/components/header/Header";
+
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
+            <head>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet"></link>
+            </head>
             <body>
+                <Header/>
                 {children}
             </body>
         </html>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header(){
+    return(
+        <header>
+            <div className="inner">
+                <div className="logo-wrap">
+                    <Image
+                        className='logo'
+                        src={'/images/header/logo.png'}
+                        width={50}
+                        height={50}
+                        alt={'로고'}
+                    />
+                </div>
+                <div className="menu-wrap">
+                    <ul>
+                        <li><Link href={'/'}>공지사항</Link></li>
+                        <li><Link href={'/'}>예약확인</Link></li>
+                        <li><Link href={'/'}>항공권찾기</Link></li>
+                        <li><Link href={'/'}>여행월드컵</Link></li>
+                    </ul>
+                    <div className="profile-wrap">
+                        <Image
+                            src={'/images/header/profile__abc.png'}
+                            width={40}
+                            height={40}
+                            alt={'profile'}
+                        />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
